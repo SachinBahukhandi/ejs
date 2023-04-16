5182353b6e7488263461eb4d90a724ed6c188066
SachinBahukhandi <sachinb0013@gmail.com>
2023-04-16 18:14:09 +0530

    Using multiple routes for the users and adding nodemon as a dependency

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a user');
+});
+
+router.get('/:id', function(req, res, next) {
+  res.json(req.params);
 });
 
 module.exports = router;
