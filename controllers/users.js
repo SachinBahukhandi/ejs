43586801822e189190ddd40aec1ef005a1905dde
SachinBahukhandi <sachinb0013@gmail.com>
2023-05-17 00:23:37 +0530

    Added validation for update user

--- controllers/users.js
+++ controllers/users.js
@@ -58,6 +58,23 @@ const validate = (method) => {
           }),
       ];
     }
+    case UPDATE_USER: {
+      return [
+        body("name", "name doesn't exists").exists(),
+        body("email", "Invalid email")
+          .exists()
+          .isEmail()
+          .custom(async (value) => {
+            const existingUser = await User.find({
+              email: value,
+            });
+            if (existingUser) {
+              // Will use the below as the error message
+              throw new Error("A user already exists with this e-mail address");
+            }
+          }),
+      ];
+    }
   }
 };
 module.exports = {
