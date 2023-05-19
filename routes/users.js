2a28f032f73409282d8f62522a98634974f4b4b5
SachinBahukhandi <sachinb0013@gmail.com>
2023-05-19 23:40:09 +0530

    Setup some more validations for update

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 var express = require("express");
 const User = require("../models/User");
 var router = express.Router();
-const {getUsers, createUser, getUser, deleteUser, validate, CREATE_USER, UPDATE_USER} = require('../controllers/users');
+const {getUsers, createUser, getUser, deleteUser, validate, CREATE_USER, UPDATE_USER, updateUser} = require('../controllers/users');
 /* GET users listing. */
 router.get("/", getUsers);
 
 router.post("/",validate(CREATE_USER) ,createUser);
 
-router.put("/",validate(UPDATE_USER) ,createUser);
+router.put("/:email",validate(UPDATE_USER) ,updateUser);
 
 router.get("/:email", getUser);
 
