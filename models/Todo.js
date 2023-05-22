05b3635079b332b9421f5c5e636e2721a7d7c200
SachinBahukhandi <sachinb0013@gmail.com>
2023-05-22 10:09:08 +0530

    Added todo model

---
+++ models/Todo.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    uid: {
+      type: String,
+      required: true,
+    },
+  })
+  module.exports = mongoose.model('Todo',todoSchema);
